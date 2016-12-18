dd76b57e851982179c85c6c164e9d572adac08f5
yogeshjoshi <joshiyogesh001@gmail.com>
2016-12-18 17:36:39 +0530

    Attemp to apply Light Sensor in app . Still Has Some problemm due to this app is crashing when we turn off the app in real device .

--- app/src/main/java/com/example/joshiyogesh/flashlight/MainActivity.java
+++ app/src/main/java/com/example/joshiyogesh/flashlight/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.joshiyogesh.flashlight;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
+import android.hardware.SensorEvent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.RelativeLayout;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     ImageButton btn;
     android.hardware.Camera camera;
     Camera.Parameters parameters;
@@ -56,8 +57,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        Sensor lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(lightSensor==null){
+            Toast.makeText(MainActivity.this,"Off!! Your Device Does not Support Light Sensors ",Toast.LENGTH_LONG).show();
+        }else{
+            sensorManager.registerListener(lightSensorEventListener,lightSensor,sensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
+    SensorEventListener lightSensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float reading = event.values[0];
+            if(reading<.5){
+                btn.setImageResource(R.drawable.unnamed);
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(parameters);
+                camera.startPreview();
+                isOn = true;
 
+            }else{
+                btn.setImageResource(R.drawable.unna);
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                camera.setParameters(parameters);
+                camera.stopPreview();
+                isOn = false;
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
     @Override
     protected void onStop() {
         super.onStop();
